d6bfcbeec69585bbbe626d443ce39fbd9902202b
vvskchaitanya <vvskchaitanya@gmail.com>
2023-09-27 17:11:06 +0000

    updated build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,37 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { TechsComponent } from './techs/techs.component';
-import { ExperienceComponent } from './experience/experience.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
-import { ThreedeComponent } from './threede/threede.component';
-import { SimplifyComponent } from './simplify/simplify.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CarouselComponent } from './carousel/carousel.component';
-import { Simplify2Component } from './simplify2/simplify2.component';
-import { StoreComponent } from './store/store.component';
-import { ConnectComponent } from './connect/connect.component';
-import { BookComponent } from './book/book.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TechsComponent,
-    ExperienceComponent,
     HeaderComponent,
     MenuComponent,
-    ThreedeComponent,
-    SimplifyComponent,
     ProfileComponent,
     CarouselComponent,
-    Simplify2Component,
-    StoreComponent,
-    ConnectComponent,
-    BookComponent,
-    DashboardComponent
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ConnectComponent } from './connect.component';
+import { FooterComponent } from './footer.component';
 
-describe('ConnectComponent', () => {
-  let component: ConnectComponent;
-  let fixture: ComponentFixture<ConnectComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConnectComponent ]
+      declarations: [ FooterComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ConnectComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
