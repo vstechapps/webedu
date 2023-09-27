eb16cf277cbb29eff2865669fa0447ede5f68ad0
vvskchaitanya <vvskchaitanya@gmail.com>
2023-09-27 12:28:54 +0000

    profile template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExperienceComponent } from './experience/experience.component';
 import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path:"",component:HomeComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TechsComponent } from './techs/techs.component';
-import { TypewriterComponent } from './typewriter/typewriter.component';
-import { AboutComponent } from './about/about.component';
-import { SkillsComponent } from './skills/skills.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { ThreedeComponent } from './threede/threede.component';
 import { SimplifyComponent } from './simplify/simplify.component';
 import { ProfileComponent } from './profile/profile.component';
-import { AwardsComponent } from './awards/awards.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { Simplify2Component } from './simplify2/simplify2.component';
 import { StoreComponent } from './store/store.component';
@@ -27,16 +23,12 @@ import { BookComponent } from './book/book.component';
     AppComponent,
     HomeComponent,
     TechsComponent,
-    TypewriterComponent,
-    AboutComponent,
-    SkillsComponent,
     ExperienceComponent,
     HeaderComponent,
     MenuComponent,
     ThreedeComponent,
     SimplifyComponent,
     ProfileComponent,
-    AwardsComponent,
     CarouselComponent,
     Simplify2Component,
     StoreComponent,
