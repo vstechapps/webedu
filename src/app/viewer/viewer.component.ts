d6e54315672e2ae90aa663212cad0abd352ae832
vvskchaitanya <vvskchaitanya@gmail.com>
2024-01-11 22:36:30 +0000

    updated pdf viewer

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-viewer',
@@ -10,8 +11,27 @@ export class ViewerComponent {
 
   url:string | null;
 
+  numPages: number = 0;
+
+  page:number = 1;
+
   constructor(private route: ActivatedRoute){
     this.url = this.route.snapshot.queryParamMap.get("url");
+  }
+
+  callBackFn(pdf: PDFDocumentProxy) {
+    // do anything with "pdf"
+    this.numPages=pdf.numPages;
+  }
+
+  next(){
+    if(this.page>=this.numPages)return;
+    this.page++;
+  }
+
+  prev(){
+    if(this.page<=1)return;
+    this.page--;
   }
 
 }
