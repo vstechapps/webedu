7214d641f8e0d87ff8e9b049386076c56b5808b9
vvskchaitanya <vvskchaitanya@gmail.com>
2024-01-27 13:29:25 +0000

    updated

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PDFDocumentProxy } from 'ng2-pdf-viewer';
+import { Utility } from '../app-utilt';
 
 @Component({
   selector: 'app-viewer',
@@ -13,10 +14,12 @@ export class ViewerComponent {
   back:string | null;
 
   numPages: number = 0;
+  isMobile:boolean = Utility.mobileAndTabletCheck();
 
   page:number = 1;
 
   constructor(private route: ActivatedRoute){
+    console.log("isMobile: "+this.isMobile);
     this.url = this.route.snapshot.queryParamMap.get("url");
     this.back = this.route.snapshot.queryParamMap.get("back");
   }
